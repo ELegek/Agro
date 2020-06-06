95432380b527bc05f4f40180b2a691fb3b343248
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2020-06-06 14:42:35 +0300

    Бургер меню

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,18 @@
 $(function(){
-
+// ================ SLIDER ===================
 $('.about__slider').slick({
     arrows: false,
     dots: true,
     fade: true,
 });
+// ================ SLIDER ===================
+
+
+// ================ MENU BURGER ===================
+document.querySelector('.menu__burger').onclick = function(){
+    this.classList.toggle('active');
+};
+// ================ MENU BURGER ===================
 
   
 });
